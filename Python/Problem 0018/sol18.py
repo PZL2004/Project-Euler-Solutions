58b1087cdbd6685f1cc70bad11b6b0dd02066f68
Pablo Zurita Lozano <pzl2004@tamu.edu>
2023-12-19 19:33:24 -0600

    sol18 done

---
+++ Python/Problem 0018/sol18.py
@@ -0,0 +1,25 @@
+"""
+Find the maximum total from top to bottom of the triangle below:
+"""
+import time
+
+start = time.time()
+
+def max_path_sum(tree):
+    for i in range(len(tree)-2, -1, -1): # last -1 very important 
+        for j in range(len(tree[i])):
+            tree[i][j] += max(tree[i+1][j], tree[i+1][j+1])
+    return tree[0][0]
+
+# change path to whatever yours is
+with open("/home/pablo/projects/Project-Euler-Solutions/Python/Problem 0018/tree.txt", "r") as f:
+    tree = []
+    for i in f:
+        tree.append(list(map(int, i.split(" ")))) #very cool
+
+if __name__ == "__main__":
+    print(max_path_sum(tree))
+    end = time.time()
+    print(f"Computation time: {1000*(end-start)} ms")
+            
+    
